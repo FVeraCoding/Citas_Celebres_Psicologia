7a7116c6ffc9b21895d8b9575072f630166ffac3
FVeraCoding <fveracoding@gmail.com>
2024-07-11 10:14:10 +0200

    first commit

---
+++ citas.js
@@ -0,0 +1,122 @@
+let citas = [
+  {
+      'autor': 'B.F.Skinner',
+      'texto': 'La principal diferencia entre ratas y personas es que las ratas aprenden de la experiencia.'
+  },
+  {
+      'autor': 'B.F.Skinner',
+      'texto': 'El auténtico problema no es si las máquinas piensan, sino si lo hacen los hombres.'
+  },
+  {
+      'autor': 'B.F.Skinner',
+      'texto': 'A menudo he dicho que mis ratas me han enseñado mucho más de lo que les he enseñado.'
+  },
+  {
+      'autor': 'B.F.Skinner',
+      'texto': 'Dame un niño y lo moldearé para cualquier cosa.'
+  },
+  {
+      'autor': 'B.F.Skinner',
+      'texto': '¿Qué es el amor, excepto otro nombre para el uso del refuerzo positivo? O viceversa.'
+  },
+  {
+      'autor': 'Abraham Maslow',
+      'texto': 'Supongo que es tentador tratar todo como si fuera un clavo, si la única herramienta que tienes es un martillo.'
+  },
+  {
+      'autor': 'Carl Gustav Jung',
+      'texto': 'Tú eres aquello que haces, no aquello que dices que harás.'
+  },
+  {
+      'autor': 'Daniel Kahneman',
+      'texto': 'Hay mucho de aleatorio en las decisiones que la gente hace.'
+  },
+  {
+      'autor': 'William James',
+      'texto': 'Mucha gente cree que están pensando cuando están reorganizando sus prejuicios.'
+  },
+  {
+      'autor': 'Carl Rogers',
+      'texto': 'La verdadera felicidad está en aceptarse uno mismo y entrar en contacto con uno mismo.'
+  },
+  {
+      'autor': 'Sigmund Freud',
+      'texto': 'A veces, un cigarro es solo un cigarro.'
+  },
+  {
+      'autor': 'Sigmund Freud',
+      'texto': 'La mente es como un iceberg, solo una séptima parte flota sobre la superficie.'
+  },
+  {
+      'autor': 'Carl Gustav Jung',
+      'texto': 'Pensar es difícil, por eso la mayoría de la gente prefiere juzgar.'
+  },
+  {
+      'autor': 'Albert Ellis',
+      'texto': 'Las mejores años de tu vida ocurren cuando decides que tus problemas son solo tuyos.'
+  },
+  {
+      'autor': 'Albert Bandura',
+      'texto': 'Las personas no solo ganan conocimiento a través de la experiencia, también aprenden observando las acciones de los demás.'
+  },
+  {
+      'autor': 'Erich Fromm',
+      'texto': 'El amor es la única respuesta sana y satisfactoria al problema de la existencia humana.'
+  },
+  {
+      'autor': 'Lev Vygotsky',
+      'texto': 'A través de otros nos convertimos en nosotros mismos.'
+  },
+  {
+      'autor': 'Martin Seligman',
+      'texto': 'El optimismo es el credo del éxito.'
+  },
+  {
+      'autor': 'Abraham Maslow',
+      'texto': 'Lo que un hombre puede ser, debe ser.'
+  },
+  {
+      'autor': 'Viktor Frankl',
+      'texto': 'Cuando no podemos cambiar una situación, tenemos el desafío de cambiarnos a nosotros mismos.'
+  },
+  {
+      'autor': 'Aaron T. Beck',
+      'texto': 'Si bien no puedes controlar los eventos que te suceden, puedes controlar tus actitudes hacia esos eventos.'
+  },
+  {
+      'autor': 'Carl Gustav Jung',
+      'texto': 'Tu visión se aclarará solo cuando puedas mirar en tu propio corazón. Porque quien mira hacia afuera, sueña; quien mira hacia adentro, despierta.'
+  },
+  {
+      'autor': 'B.F. Skinner',
+      'texto': 'El problema con la libertad es que pocas personas saben qué hacer con ella.'
+  },
+  {
+      'autor': 'Sigmund Freud',
+      'texto': 'El primer humano que insultó a su enemigo en lugar de tirarle una piedra fue el fundador de la civilización.'
+  },
+  {
+      'autor': 'Carl Rogers',
+      'texto': 'Lo curioso es que cuando me acepto a mí mismo tal como soy, entonces puedo cambiar.'
+  },
+  {
+      'autor': 'Albert Ellis',
+      'texto': 'Deja de tener miedo de hacer cosas equivocadas, porque la única manera en que podrías no cometer errores es no hacer nada en absoluto, lo cual sería el mayor error.'
+  },
+  {
+      'autor': 'Sigmund Freud',
+      'texto': 'Donde está el ello, estará el yo.'
+  },
+  {
+      'autor': 'Martin Seligman',
+      'texto': 'La psicología positiva no es solo acerca de la felicidad, sino también de la fuerza, el coraje y el crecimiento personal.'
+  },
+  {
+      'autor': 'Daniel Kahneman',
+      'texto': 'La pereza es el motor del progreso, la necesidad de minimizar el esfuerzo explica muchas decisiones.'
+  },
+  {
+      'autor': 'Lev Vygotsky',
+      'texto': 'La mayor parte de lo que un niño aprende se adquiere a través de la interacción social.'
+  }
+];
